0b9b6dacda8315f7d2ef9e5eeff3d22134b9428c
honjow <honjow311@gmail.com>
2025-07-15 00:15:15 +0800

    fix file permissions

--- chimeraos/profiledef.sh
+++ chimeraos/profiledef.sh
@@ -17,6 +17,7 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/install.sh"]="0:0:755"
+  ["/root/install-init.sh"]="0:0:755"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
