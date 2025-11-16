1ba6f6b319b89db2071d690d42cb661a0bb903b1
honjow <honjow311@gmail.com>
2025-11-16 16:19:38 +0800

    feat: update additional package URLs in build-iso.sh to version 6.16.4-101 for SkorionOS

--- build-iso.sh
+++ build-iso.sh
@@ -28,8 +28,8 @@ AUR_PACKAGES="\
 "
 
 ADDITIONAL_PACKAGES="\
-    https://github.com/SkorionOS/linux-skorion/releases/download/v6.16.4-1/linux-skchos-6.16.4-1-x86_64.pkg.tar.zst \
-	https://github.com/SkorionOS/linux-skorion/releases/download/v6.16.4-1/linux-skchos-headers-6.16.4-1-x86_64.pkg.tar.zst \
+    https://github.com/SkorionOS/linux-skorion/releases/download/v6.16.4-101/linux-skchos-6.16.4-101-x86_64.pkg.tar.zst \
+	https://github.com/SkorionOS/linux-skorion/releases/download/v6.16.4-101/linux-skchos-headers-6.16.4-101-x86_64.pkg.tar.zst \
 "
 
 # create repo directory if it doesn't exist yet
