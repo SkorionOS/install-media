7eb8f98fddf29dbf66cf9ab55812dde34379e97d
honjow <honjow311@gmail.com>
2026-03-18 12:02:25 +0800

    update for new frzr

--- skorionos/airootfs/usr/local/lib/installer/config.py
+++ skorionos/airootfs/usr/local/lib/installer/config.py
@@ -24,7 +24,7 @@ class Config:
         self.ui_scale = float(ui_scale)
         
         # Version
-        self.version = "3.4.0"
+        self.version = "3.5.0"
         
         # Installation paths
         self.mount_path = "/tmp/frzr_root"
